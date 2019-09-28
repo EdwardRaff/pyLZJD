494fc98f0c4b9fd6104aadba0be2f27a159e9596
Maksim Eren <meren1@umbc.edu>
2019-09-27 22:30:00 -0400

    Fixed the X_hashes var bug. Tested the code, commiting the working version.

--- examples/t5 Corpus v2.py
+++ examples/t5 Corpus v2.py
@@ -8,6 +8,7 @@ Created on Fri Sep 20 18:11:39 2019
 # LIBRARIES NEEDED
 import os
 import glob
+import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 from pyLZJD import digest, sim
@@ -18,12 +19,14 @@ from sklearn.neighbors import KNeighborsClassifier
 
 
 # SETTINGS
-DATASET_FILE_NAME = 't5-corpus.zip'
-DATASET_URL       = 'http://roussev.net/t5/t5-corpus.zip'
-TITLE             = 'TSNE Visualization'
-PLOT_FILE_NAME    = "t5_perp5.pdf"
-PATH_NAME_PATTERN = "t5/*"
-PROCESSES         = -1
+DATASET_FILE_NAME  = 't5-corpus.zip'
+DATASET_FILE_UNZIP = 't5'
+DATASET_URL        = 'http://roussev.net/t5/t5-corpus.zip'
+TITLE              = 'TSNE Visualization'
+PLOT_FILE_NAME     = "t5_perp5.pdf"
+PATH_NAME_PATTERN  = "t5/*"
+PROCESSES          = -1
+X_HASHES           = None
 
 
 #==============================================================================
@@ -38,26 +41,27 @@ def driver():
     X_paths, labels_true, Y = collect_files()
     
     # hash, digest files
-    X_hashes = hash_files(X_paths)
+    hash_files(X_paths)
     
     # get 1D vector
-    X = create_1D_vector(X_hashes)
+    X = create_1D_vector()
     
     # train the model
-    train(X, Y, X_hashes)
+    train(X, Y)
     
     # plot the model
-    plot(labels_true, X, Y, X_hashes)
-    
-    
-    
+    plot(labels_true, X, Y)
+ 
+
+
 #==============================================================================
 # Downloads the dataset needed
 #==============================================================================      
 def download_dataset():
     
     # First, lets check if we have the t5 corpus
-    if not (os.path.exists(DATASET_FILE_NAME)):
+    if not (os.path.exists(DATASET_FILE_NAME) or
+            os.path.exists(DATASET_FILE_UNZIP)):
         print("Downloading ", DATASET_FILE_NAME)
         import urllib.request
         try:
@@ -66,10 +70,11 @@ def download_dataset():
             print("Error while downloading the dataset")
             print(e)
 	
-    # Unzip the dataset
-    import zipfile
-    with zipfile.ZipFile(DATASET_FILE_NAME,"r") as zip_ref:
-        zip_ref.extractall(".")
+        # Unzip the dataset
+        import zipfile
+        print("Extracting ", DATASET_FILE_NAME)
+        with zipfile.ZipFile(DATASET_FILE_NAME,"r") as zip_ref:
+            zip_ref.extractall(".")
         
 
 
@@ -78,9 +83,10 @@ def download_dataset():
 #==============================================================================  
 def collect_files():
     '''
-    Lets collect up all the files in the t5 corpus. Its organized as one big folder,
-    and the extension of each file tells us what kind of file it is. 
+    Lets collect up all the files in the t5 corpus. Its organized as one big 
+    folder, and the extension of each file tells us what kind of file it is. 
     '''
+    print("Collecting labels...")
     X_paths = glob.glob(PATH_NAME_PATTERN)
     
     labels_true = list(set([ x[x.find(".")+1:] for x in X_paths]))
@@ -97,44 +103,46 @@ def collect_files():
 # Take the hash of the files
 #============================================================================== 
 def hash_files(X_paths):
+    global X_HASHES
     '''
     Lets hash all the files now! We have a list of paths, 
     pyLZJD can take that dirrectly and convert it to hashes
     '''
-    X_hashes = digest(X_paths, processes=PROCESSES)
+    print("Generating hash of all the files...")
+    X_HASHES = digest(X_paths, processes=PROCESSES)
     print("Done hashing!")
-    
-    return X_hashes
     
 
 
 #==============================================================================
 # Creates a vector
 #============================================================================== 
-def create_1D_vector(X_hashes):
+def create_1D_vector():
+    global X_HASHES
     '''
     We are going to use some tools from scikit-learn. 
     It needs a distance function between data stored as a list of vectors. 
     So we will create a list of 1-D vectors, 
     each each vector sotres the index to it's hash in X_hashes
     '''
-    X = [ [i] for i in range(len(X_hashes))]
+    print("Generating 1D vector...")
+    X = [[i] for i in range(len(X_HASHES))]
     
     return X
-
 
 
 
 #==============================================================================
 # Compute distence between two factors
 #============================================================================== 
-def lzjd_dist(a, b, **X_hashes):
+def lzjd_dist(a, b):
+    global X_HASHES
     '''
     Now we define a distance function between two vectors in X. 
     It accesses the index value, and computes the LZJD distance
     '''
-    a_i = X_hashes[int(a[0])]
-    b_i = X_hashes[int(b[0])]
+    a_i = X_HASHES[int(a[0])]
+    b_i = X_HASHES[int(b[0])]
 	
     return 1.0-sim(a_i, b_i)
 
@@ -143,11 +151,12 @@ def lzjd_dist(a, b, **X_hashes):
 #==============================================================================
 # Train the model
 #============================================================================== 
-def train(X, Y, X_hashes):
+def train(X, Y):
+    print("Training the ML model...")
+    
     knn_model = KNeighborsClassifier(n_neighbors=5, \
                                      algorithm='brute', \
-                                     metric=lzjd_dist, \
-                                     metric_params=X_hashes)
+                                     metric=lzjd_dist)
     
     scores = cross_val_score(knn_model, X, Y, cv=5)
     print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), \
@@ -158,11 +167,12 @@ def train(X, Y, X_hashes):
 #==============================================================================
 # Plot the model
 #============================================================================== 
-def plot(labels_true, X, Y, X_hashes):
+def plot(labels_true, X, Y):
+    print("Plotting the model...")
+    
     X_embedded = TSNE(n_components=2, \
                       perplexity=5, \
-                      metric=lzjd_dist, \
-                      metric_params=X_hashes).fit_transform(X)
+                      metric=lzjd_dist).fit_transform(X)
     
     # define the colors to be used in the plot
     colors = [plt.cm.Spectral(each) \
@@ -192,4 +202,3 @@ def plot(labels_true, X, Y, X_hashes):
 # START
 if __name__ == "__main__":
     driver()
-    
